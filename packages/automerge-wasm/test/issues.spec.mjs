b484cc7259344207b6777449f1e2b8506ae23171
Nemanja Tosic <nemanja@livingspec.com>
2021-03-31 19:38:31 +0200

    Uncomment tests

--- packages/automerge-wasm/test/issues.spec.mjs
+++ packages/automerge-wasm/test/issues.spec.mjs
@@ -20,7 +20,7 @@ describe("text issues", () => {
 
   context("text field with more that 63", () => {
     [1, 28, 63, 64, 100, 127, 128, 191, 192, 256, 383, 384, 511, 512].forEach(length => {
-      it.only("should show wrens", () => {
+      it("should show wrens", () => {
         expect(
           toJs(
             Automerge.from({
